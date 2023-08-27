94548d4fd818d2a71451f17d945d0d80bd6f8a42
InspectorCaracal <51038201+InspectorCaracal@users.noreply.github.com>
2023-08-27 10:38:01 -0600

    fix fstring

--- server/portal/text2html.py
+++ server/portal/text2html.py
@@ -20,7 +20,7 @@ class ModdedHTMLParser(TextToHTMLparser):
 
         """
         cmd, text = [grp.replace('"', "\\&quot;") for grp in match.groups()]
-        val = r'<span class="mxplink" data-command="{cmd}">{text}</span>'
+        val = rf'<span class="mxplink" data-command="{cmd}">{text}</span>'
         return val
 
 
